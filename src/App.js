ca0c06331ef20fc4e39cc4165469d84fd2570e34
Kyoo130 <kml9003@kakao.com>
2022-02-24 01:10:19 +0900

    ♻️ Router 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import New from "./pages/New";
 
 function App() {
   return (
-    <div className="App">
-      메인 페이지
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/new" element={<New />}/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
