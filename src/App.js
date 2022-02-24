a6894f6e90888d16d19e4df8f57d995935a33b3a
Danim Kim <kdn725@gmail.com>
2022-02-25 01:12:50 +0900

    💄 뷰페이지 css 작업

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import New from "./pages/New";
+import ViewPage from './pages/ViewPage';
 
 function App() {
   const [data, setData] = useState([]);
@@ -31,6 +32,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home dataList={data} />}/>
           <Route path="/new" element={<New onCreate={onCreate} />}/>
+          <Route path="/view" element={<ViewPage />}/>
           <Route path="*" element={<NotFound />}/>
         </Routes>
       </div>
