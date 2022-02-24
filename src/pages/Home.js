577604687b82a651e5a7bccd461b380aac3cca11
Kyoo130 <kml9003@kakao.com>
2022-02-24 14:34:23 +0900

    :sparkles: 글쓰기 버튼 생성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom"
 
-const Home = () => {
+const Home = ({dataList}) => {
+  const navigate = useNavigate();
+
   return (
     <div>
       홈 화면입니다.
+      <div>
+        <button onClick={() => {navigate("/new")}}>글쓰기</button>
+      </div>
     </div>
   );
 };
