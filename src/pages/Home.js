f1de503c8ccf593c3d43b971739676e47da08c3f
Kyoo130 <kml9003@kakao.com>
2022-02-25 13:07:20 +0900

    :fire: 글쓰기, 게시글이동 버튼 삭제

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 import ListItem from "../components/ListItem";
 
 const Home = ({ dataList }) => {
-  const navigate = useNavigate();
 
   return (
     <div>
-      <Header />
-      <div>
-        <button
-          onClick={() => {
-            navigate("/new");
-          }}
-        >
-          글쓰기
-        </button>
-        <button
-          onClick={() => {
-            navigate("/view");
-          }}
-        >
-          게시글이동
-        </button>
-      </div>
       {dataList.map((it) => {
         return <ListItem key={it.id} {...it} />;
       })}
-      <Footer />
     </div>
   );
 };
