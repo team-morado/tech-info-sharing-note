b5bba7f080618f57e66d177cfd30577f825fe178
mijin <mmcho122@gmail.com>
2022-02-26 21:05:51 +0900

    🩹  import 방식 변경

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 // components export
 
 import Footer from "./Footer";
-import {Header, HeaderNew} from "./Header";
+import { Header } from "./Header";
 import ListItem from "./ListItem";
 import ListOption from "./ListOption";
-import Svg from "./Svg";
+import * as Svg from "./Svg";
 import ViewCard from "./ViewCard";
 
-export {Footer, Header, ListItem, ListOption, Svg, ViewCard, HeaderNew};
+export { Footer, Header, ListItem, ListOption, Svg, ViewCard };
