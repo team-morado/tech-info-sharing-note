d60d4370db4622322ea4e08522215e12b921a243
mijin <mmcho122@gmail.com>
2022-02-27 23:54:51 +0900

    ✨ 뒤로가기 버튼 표시 기능 추가

---
+++ src/elements/NextBtn.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { EditIcon } from "../components/Svg";
+
+const Next = () => {
+  const location = useLocation();
+  return (
+    <Button to="/new" current={location.pathname === "/"}>
+      <EditIcon />
+      <BtnText>글쓰기</BtnText>
+    </Button>
+  );
+};
+
+const Button = styled(Link)`
+  display: ${(props) => (props.current ? "flex" : "none")};
+  align-items: center;
+  background-color: #8465e1;
+  color: #fff;
+  cursor: pointer;
+  padding: 0.3em 0.3rem;
+  margin-right: 0.7rem;
+  border-radius: 4px;
+  font-size: 1rem;
+`;
+
+const BtnText = styled.p`
+  margin: 0 0.3rem;
+  letter-spacing: -0.045rem;
+`;
+
+export default Next;
