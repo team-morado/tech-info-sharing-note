5217da7decb27db79cb9e80207902c6680029014
Kyoo130 <kml9003@kakao.com>
2022-03-01 15:00:23 +0900

    :sparkles: redux 적용 코드 커밋

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {useSelector} from "react-redux"
+
 import ListItem from "../components/ListItem";
 import ListOptionGroup from "../components/ListOptionGroup";
 
-const Home = ({ dataList }) => {
-
+const Home = () => {
+  const dataList = useSelector((state) => state.techInfo.list);
   return (
     <div>
       <ListOptionGroup />
