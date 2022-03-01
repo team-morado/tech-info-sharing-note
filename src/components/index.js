e2438849e4fc0372ed3d7e4fd599bafaed52222b
Kyoo130 <kml9003@kakao.com>
2022-03-01 23:12:19 +0900

    :dizzy: CommonFunc import 코드 추가

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,6 @@ import ListItem from "./ListItem";
 import ListOption from "./ListOption";
 import * as Svg from "./Svg";
 import ClipboardCopy from "./ClipboardCopy";
+import CommonFunc from "./Common";
 
-export { Footer, Header, ListItem, ListOption, Svg, ClipboardCopy };
+export { Footer, Header, ListItem, ListOption, Svg, ClipboardCopy, CommonFunc };
