88b0febb3c7a6bb80c1a7c9533ea1d2dfcf2b30e
mijin <mmcho122@gmail.com>
2022-03-05 21:27:03 +0900

    ✨ Button 컴포넌트 추가

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../elements";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <NotFoundCont>
       <ErrorTxt>404</ErrorTxt>
-      <SubTxt>서버 주소를 다시 확인해주세요.</SubTxt>
+      <SubTxt>서버 주소를 다시 확인해주세요 :(</SubTxt>
+      <Button width="125px" _onClick={() => navigate(-1)}>
+        이전 페이지
+      </Button>
     </NotFoundCont>
   );
 };
@@ -31,5 +37,5 @@ const SubTxt = styled.p`
   font-size: 16px;
   letter-spacing: -0.045rem;
   color: #c4c4c4;
-  margin-top: 20px;
+  margin: 20px 0;
 `;
