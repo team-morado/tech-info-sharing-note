41b4bdc9f524e554bda7c8f98cec7b7b4e19fecd
Kyoo130 <kml9003@kakao.com>
2022-03-06 14:05:49 +0900

    :ambulance: dummy data 적용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux"
 
@@ -8,11 +8,38 @@ const Home = () => {
   const navigate = useNavigate();
   const techInfo_list = useSelector((state) => state.techInfo.list);
 
+  const [infoData, setInfoData] = useState([
+    { id: 1,
+      author: "지윤",
+      category: "JS",
+      content: "공유하고싶다",
+      created_date: 1646313227990,
+      title: "테스트테스트",
+      url: "dgfdg"
+    },
+    { id: 2,
+      author: "규민",
+      category: "React",
+      content: "공부를 합시다",
+      created_date: 1646313227990,
+      title: "테스트테스트",
+      url: "asdasdas"
+    },
+    { id: 3,
+      author: "다님",
+      category: "CSS",
+      content: "디자인을 만들어보자",
+      created_date: 1646313227990,
+      title: "테스트테스트",
+      url: "dgfdg"
+    },
+  ]);
+
   return (
     <div>
       <ListOptionGroup />
       {
-        techInfo_list.map((it, index) => <ListItem key={it.id} {...it} _onClick={() => {navigate('/view/' + index)}} />)
+        infoData.map((it, index) => <ListItem key={it.id} {...it} _onClick={() => {navigate('/view/' + index)}} />)
       }
     </div>
   );
