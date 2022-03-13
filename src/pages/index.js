5ad98a6e7fd89b09217c31b0c011b29aeb56b07e
Kyoo130 <kml9003@kakao.com>
2022-03-13 23:30:28 +0900

    :sparkles: Loading import 코드 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,6 @@ import NotFound from "./NotFound";
 import Splash from "./Splash";
 import ViewPage from "./ViewPage";
 import Edit from "./Edit";
+import Loading from "./Loading";
 
-export {Home, New, NotFound, Splash, ViewPage, Edit};
+export {Home, New, NotFound, Splash, ViewPage, Edit, Loading};
