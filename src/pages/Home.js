ff2ffbb5fef6fb0a073f78994858490439a36846
mijin <mmcho122@gmail.com>
2022-03-13 17:07:05 +0900

    ⚰️ 사용하지 않는 컴포넌트 삭제

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {ListItem, ListOptionGroup} from "../components";
 import {addCategoryFB, loadTechInfoFB, removeCategoryFB} from "../shared/redux/modules/techInfo";
-import Splash from "./Splash";
 
 const Home = () => {
 
@@ -59,7 +58,6 @@ const Home = () => {
 
   return (
     <div>
-      <Splash />
       <ListOptionGroup addChecked={addChecked} removeChecked={removeChecked}/>
       {techInfo_list.sort((a,b) => b.created_date-a.created_date).map((it, index) => {
         return (
